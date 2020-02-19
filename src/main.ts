80c19c327c418271d4f9758174b24875c45b9a19
Gregory Anne <ganne@effisoft-group.com>
2020-02-19 09:42:29 +0100

    generation des types ts dans le paquet publié

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { CalculationModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(CalculationModule);
   await app.listen(3000);
 }
 bootstrap();
